f46e478c9e30afb3ee070382200c8cd218e8eee1
Chelosky <marcelolazo19972013@hotmail.com>
2022-06-25 23:27:36 -0400

    :bug: fixed with headless false gui

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,13 +11,10 @@ export class AppService {
   async test(): Promise<string[]> {
     puppeteer.use(Adblocker({ blockTrackers: true }));
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: false,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
-    await page.setUserAgent(
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
-    );
     const url = 'https://lectortmo.com/library/manga/9276/one-punch-man';
     await page.goto(url);
 
@@ -30,13 +27,17 @@ export class AppService {
       return aTag?.getAttribute('href');
     });
 
+    let client = await page.target().createCDPSession();
+    await client.send('Network.clearBrowserCookies');
+    await client.send('Network.clearBrowserCache');
+
     const page2 = await browser.newPage();
     await page2.goto(mangaUrl!);
 
     let currentUrl = await page2.url();
     currentUrl = currentUrl.replace('paginated', 'cascade');
 
-    const client = await page2.target().createCDPSession();
+    client = await page2.target().createCDPSession();
     await client.send('Network.clearBrowserCookies');
     await client.send('Network.clearBrowserCache');
 
